fe6d6bb0346b85a960a9f058c7c06baf17d6100b
vasker <yarinvasket@gmail.com>
2021-10-29 23:27:07 +0300

    implement rel2abs

---
+++ scripts/rel2abs.py
@@ -0,0 +1,28 @@
+# Turn the relative paths to absolute paths in a .m3u playlist
+
+import sys
+import os.path as path
+from os import linesep as endl
+
+# Check if a file was given
+if len(sys.argv) < 2:
+    print("Usage: python rel2abs.py [FILE]")
+    exit(1)
+
+# Open the file
+f = open(sys.argv[1], 'rw')
+
+lines = f.readlines()
+
+# str of new file
+buffer = ""
+
+# Replace the path lines with their absolute path
+for line in lines:
+    if line[0] != '#':
+        line = path.abspath(line)
+    buffer += line + endl
+
+# Write the buffer to the file
+f.write(buffer)
+f.close()
